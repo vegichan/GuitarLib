7637f4312604dacbfe07840e2af3aa68134575ae
Martin Kysel <mkysel@nuodb.com>
2017-08-21 11:16:01 +0100

    add header

--- src/main/java/com/martinkysel/guitarlib/tester/NoteGenerator.java
+++ src/main/java/com/martinkysel/guitarlib/tester/NoteGenerator.java
@@ -1,5 +1,23 @@
 package com.martinkysel.guitarlib.tester;
 
+/**
+ GuitarLib
+ Copyright (C) 2017 Martin Kysel
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 import com.martinkysel.guitarlib.basics.Note;
 import com.martinkysel.guitarlib.instruments.Guitar;
 import com.martinkysel.guitarlib.tunings.StandardTuning;
@@ -47,10 +65,10 @@ public class NoteGenerator {
     private Guitar g = new Guitar(t);
 
     private int lowFret = 0;
-    private int highFret = 4;
+    private int highFret = 0;
 
-    private boolean[] useString = {true, false, false, false, false, false};
-    //private boolean[] useString = {true, true, true, true, true, true};
+    //private boolean[] useString = {false, false, false, true, true, true};
+    private boolean[] useString = {true, true, true, true, true, true};
 
     Random rand = new Random();
 }
